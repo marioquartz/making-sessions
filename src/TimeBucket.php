39901fde1500cb962bef61e3aa3c443f94fda696
Mario <marioquartz@gmail.com>
2021-06-27 11:00:57 +0200

    When using DateTimeInmutable the variable is not an int.

--- src/TimeBucket.php
+++ src/TimeBucket.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Marioquartz\MakingSessions;
 
@@ -12,41 +13,33 @@ class TimeBucket
 
     /**
      * @param int|\DateTimeImmutable $start
-     *
-     * @return TimeBucket
      */
     public function setStart($start): TimeBucket
     {
-        if ($start instanceof \DateTimeImmutable) {
-            $this->start=$start->format("U");
+        if (($start instanceof \DateTimeImmutable) === true) {
+            $this->start = (int) $start->format('U');
+            return $this;
         }
-        $this->start=$start;
+        $this->start = $start;
         return $this;
     }
 
     /**
      * @param int|\DateTimeImmutable $end
-     *
-     * @return TimeBucket
      */
     public function setEnd($end): TimeBucket
     {
         if ($end instanceof \DateTimeImmutable) {
-            $this->end=$end->format("U");
+            $end = (int) $end->format('U');
         }
-        if ($end<$this->start) {
-            $end=$this->start;
+        if ($end < $this->start) {
+            $end = $this->start;
         }
-        $this->end=$end;
-        $this->duration=$this->end-$this->start;
+        $this->end = $end;
+        $this->duration = $this->end - $this->start;
         return $this;
     }
 
-    /**
-     * @param int $duration
-     *
-     * @return TimeBucket
-     */
     public function setDuration(int $duration): TimeBucket
     {
         $this->duration = $duration;
@@ -54,44 +47,27 @@ class TimeBucket
         return $this;
     }
 
-    /**
-     * @param string $type
-     *
-     * @return TimeBucket
-     */
     public function setType(string $type): TimeBucket
     {
         $this->type = $type;
         return $this;
     }
 
-    /**
-     * @return int
-     */
     public function getStart(): int
     {
         return $this->start;
     }
 
-    /**
-     * @return int
-     */
     public function getEnd(): int
     {
         return $this->end;
     }
 
-    /**
-     * @return int
-     */
     public function getDuration(): int
     {
         return $this->duration;
     }
 
-    /**
-     * @return string
-     */
     public function getType(): string
     {
         return $this->type;
